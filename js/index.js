773cd28ea725ebf8f9c54edff3e48390eb81f3a3
dcooper5 <47071908+dcooper5@users.noreply.github.com>
2019-04-29 23:34:09 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//Below is the code for a console-box that documents what a user clicks on the sidebar, although not all buttons have links it will still register what the user has clicked. 
+
+$(function(){
+  $('#console-box').append('Console is ready:');
+  
+  $('li').click(function(){
+    $('#console-box').append('<br>class:'+$(this).attr("class")+'|value: '+$(this).text()+'.');
+    $(this).toggleClass('selected').siblings().removeClass('selected');
+; 
+  });
+  
+});
