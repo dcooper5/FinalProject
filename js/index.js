93c7ee13ce426e1aa7631f53026f3689695380e6
dcooper5 <47071908+dcooper5@users.noreply.github.com>
2019-04-30 14:47:51 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(function(){
   $('#console-box').append('Console is ready:');
   
   $('li').click(function(){
-    //$('#console-box').append('<br>class:'+$(this).attr("class")+'|value: '+$(this).text()+'.');
+    $('#console-box').append('<br>class:'+$(this).attr("class")+'|value: '+$(this).text()+'.');
     $(this).toggleClass('selected').siblings().removeClass('selected');
 ; 
     $('#console-box').append('<br>{"Selection" : "'+$(this).text()+'"}');
